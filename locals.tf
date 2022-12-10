395f71a8431ce78f8877fe6ca045a32a33291e42
dedie@CALLMT22223 <daniel.edie@gettyimages.com>
2022-12-10 14:05:55 -0700

    testing automation git push

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   common_tags={
-    company = "jjtech"
+    company = "jjtech-test"
     owner = "jjtech DevOps Team"
     team-email = "team-devops@jjtech.com"
     time = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())    
